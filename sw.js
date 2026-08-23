79c8eb88619808661bd068685da26bfd9c3ffeae
03shiotaka07 <eldest0307@gmail.com>
2026-08-23 21:24:12 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'runlog-v6';
+const CACHE_NAME = 'runlog-v7';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
